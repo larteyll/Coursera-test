20675d91b1d050f2358a36527a5bc776076c99e3
larteyll <134552664+larteyll@users.noreply.github.com>
2023-05-24 21:52:39 +0000

    upload files

---
+++ script.js
@@ -0,0 +1,38 @@
+const a = Number(prompt(`enter any number`));
+const b = Number(prompt(`enter any number`));
+// console.log(a + b);
+
+function add(a, b) {
+  return a + b;
+}
+
+const c = add(a, b);
+
+console.log(c);
+
+function mul(a, b) {
+  return a * b;
+}
+
+const d = mul(a, b);
+
+console.log(d);
+
+function divs(a, b) {
+  return a / b;
+}
+
+const e = divs(a, b);
+console.log(e);
+
+function minus(a, b) {
+  return a - b;
+}
+const f = minus(a, b);
+console.log(f);
+
+function power(a, b) {
+  return a ** b;
+}
+const h = power(a, b);
+console.log(h);
